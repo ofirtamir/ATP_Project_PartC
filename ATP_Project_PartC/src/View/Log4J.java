0c6f8c182caa7e92442fca07b261e782c60c30f9
ofirtamir <127851334+ofirtamir@users.noreply.github.com>
2023-06-21 17:41:44 +0300

    Delete ATP_Project_PartC directory

--- ATP_Project_PartC/src/View/Log4J.java
+++
@@ -1,8 +0,0 @@
-package View;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class Log4J {
-    public static final Logger LOG = LogManager.getLogger();
-}
